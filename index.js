cdcd3ac3a6730b3322ed13627e2afe6975c9af3c
ayrtonsilas <ayrtonsilasguimaraes@gmail.com>
2019-08-14 19:21:53 -0300

    teste

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const app = express();
 //permite que envie dados para aplicação no formato json
 app.use(express.json());
 app.use(cors());
-// app.use(cors({
-//     origin:['http://localhost:8080'],
-//     methods:['GET','POST'],
-//     credentials: true // enable set cookie
-// }));
 
 // requireDir('./src/api');
 

6dbc00893a927469cb5ba85cfca114070c1a6cad
ayrtonsilas <ayrtonsilasguimaraes@gmail.com>
2019-08-14 01:33:19 -0300

    teste

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const requireDir = require("require-dir");
 const app = express();
 //permite que envie dados para aplicação no formato json
 app.use(express.json());
-app.use(cors({
-    origin:['http://localhost:8080'],
-    methods:['GET','POST'],
-    credentials: true // enable set cookie
-}));
+app.use(cors());
+// app.use(cors({
+//     origin:['http://localhost:8080'],
+//     methods:['GET','POST'],
+//     credentials: true // enable set cookie
+// }));
 
 // requireDir('./src/api');
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,4 +5,8 @@ const login = require('../api/login');
 
 routes.get('/login/:cpf/:senha',login.login);
 
+routes.get('/',(req,res) => {
+    return res.send('wellcome');
+})
+
 module.exports = routes;

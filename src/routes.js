4b1f43f8ab1a63141a03b93d1ab73c41b5a72c07
ayrtonsilas <ayrtonsilasguimaraes@gmail.com>
2020-03-08 14:53:15 -0300

    ajuste

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = express.Router();
-
+const path = require('path');
 const getDados = require('./api/get-dados');
 const getInfoMateria = require('./api/get-info-materia');
 
@@ -8,14 +8,17 @@ routes.post('/get-dados', getDados.execute);
 routes.get('/get-info-materia/:disciplina/:nuPerInicial', getInfoMateria.execute);
 routes.get('/verifica-atualizacao', (req, res) => {
     return res.send({
-        ULTIMA_ATT : 1583647657864,
-        ENDERECO_ATT : 'https://ayrtonsilas.com.br/api/download-apk'
+        ULTIMA_ATT: 1583647657864,
+        ENDERECO_ATT: 'https://ayrtonsilas.com.br/api/download-apk'
     })
+})
+routes.get('/apk',async (req,res) => {
+    res.sendFile(path.join(__dirname+'/view/apk.html'));
 })
 routes.get('/download-apk', async (req, res) => {
     const file = `src/apk/siac-mobile.apk`;
-    res.download(file); // Set disposition and send it.
-  });
+    res.download(file);
+});
 
 routes.get('/', (req, res) => {
     return res.send('wellcome');
